589262e5cde0061f98ff9f3bf5084f645f445b04
fi9ish <tima.finish@gmail.com>
2022-11-03 12:33:17 +0100

    Change <a> to next.js <Image>

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import { FaTelegramPlane } from "react-icons/fa"
 import { BsTwitter } from "react-icons/bs"
 import { FiGithub } from "react-icons/fi"
+import Image from "next/image";
 
 function Footer(props) {
 
@@ -10,9 +11,9 @@ function Footer(props) {
     <div className="Footer">
       <p>Made by @fi9ish</p>
       <span className="Footericons">
-        <a className="profileLink" href="https://twitter.com/FinishMee" ><FaTelegramPlane size={iconSize}></FaTelegramPlane></a>
-        <a className="profileLink" href="https://t.me/fi9ish"><BsTwitter size={iconSize}></BsTwitter></a>
-        <a className="profileLink" href="https://github.com/fi9ish/"><FiGithub size={iconSize}></FiGithub></a>
+        <Image className="profileLink" href="https://twitter.com/FinishMee" alt="photo"><FaTelegramPlane size={iconSize}></FaTelegramPlane></Image>
+        <Image className="profileLink" href="https://t.me/fi9ish" alt="photo"><BsTwitter size={iconSize}></BsTwitter></Image>
+        <Image className="profileLink" href="https://github.com/fi9ish/" alt="photo"><FiGithub size={iconSize}></FiGithub></Image>
       </span>
     </div>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import { AiFillGithub } from "react-icons/ai"
 
 function Navbar(props) {
   return (
     <div className="Navbar">
       <h3>Sudoku Game</h3>
-      <button><a href="https://github.com/fi9ish/sudoku.js">Source code</a> <AiFillGithub size={24}></AiFillGithub></button>
+      <button><Image href="https://github.com/fi9ish/sudoku.js" alt="photo">Source code</Image> <AiFillGithub size={24}></AiFillGithub></button>
     </div>
   );
 }
